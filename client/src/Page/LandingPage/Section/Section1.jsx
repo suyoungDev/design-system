fc7a6ff76ee84d0d5feda3a5663ad59b9e56aae0
mocha <syda07@gmail.com>
2021-04-18 18:45:15 +0900

    🔥 logo 삭제

--- client/src/Page/LandingPage/Section/Section1.jsx
+++ client/src/Page/LandingPage/Section/Section1.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Inline, Outline, MainTitle } from '../LandingPage.styles';
-import Logo from '../../../Components/Logo';
 import Section1Elem from './Section1Elem';
 
 const Section1 = ({ offset }) => {
@@ -14,7 +13,6 @@ const Section1 = ({ offset }) => {
           </Inline>
         </h2>
       </MainTitle>
-      <Logo />
     </Container>
   );
 };
