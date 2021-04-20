84e373e1676ab490f784c54450c2962467b37558
mocha <syda07@gmail.com>
2021-04-21 07:13:54 +0900

    🐛 ul 남용한거 수정

--- client/src/Page/BaseColor/Section/BaseColorList.styles.jsx
+++ client/src/Page/BaseColor/Section/BaseColorList.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const ListContainer = styled.ul`
+export const ListContainer = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
